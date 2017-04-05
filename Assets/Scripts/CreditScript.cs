f0e5393ece3b6dd459467ded1c7b0cb701725cdf
Ohmeko Ocampo <ohmeko.ocampo@gmail.com>
2017-04-05 14:11:04 -0700

    Added: Art Folder, Credit Scene, Font Folder, Animation Folder, and I setup Lazy Flight in Flock2 scene

---
+++ Assets/Scripts/CreditScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditScript : MonoBehaviour {
+
+
+    public Button toMenu;
+
+	// Use this for initialization
+	void Start () {
+        toMenu = toMenu.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
