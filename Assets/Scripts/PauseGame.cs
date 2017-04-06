7c420c1aad48ac4e748b38e30123c64a923619dd
Ohmeko Ocampo <ohmeko.ocampo@gmail.com>
2017-04-05 22:44:25 -0700

    Added Pause Menu capability to both flocking modes

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class PauseGame : MonoBehaviour {
+
+    public Transform canvas;
+    
+    // Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+            
+        	
+	}
+
+    public void Pause()
+    {
+         if(canvas.gameObject.activeInHierarchy == false)
+         {
+               canvas.gameObject.SetActive(true);
+               Time.timeScale = 0;//Stop time in game.
+         }
+         else
+         {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;//Resume time in game.
+         }
+    }
+
+    public void BackToMenu()
+    {
+
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main Menu");
+        
+    }
+}
